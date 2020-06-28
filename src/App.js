2fda81495c69b11fb76cfc68477e21c49fd25cec
frozenbanana <henrybergstrom@protonmail.com>
2020-06-28 22:21:23 +0200

    first commit, cards rendering

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { NavigationBar } from "./components/NavigationBar";
+import { About } from "./components/About";
+import { Gallery } from "./components/Gallery";
+import { Shop } from "./components/Shop";
+
+import "./App.css";
+
+// In the code below, BlogPost is used as both a <Route component>
+// and in a <Route render> function. In both cases, it receives a `match`
+// prop, which it uses to get the URL params.
+// function BlogPost({ match }) {
+//     let { slug } = match.params;
+//     // ...
+// }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <NavigationBar title="Maija Papazi" />
+                <Switch>
+                    {/* Using the `component` prop */}
+                    <Route path="/" component={Gallery} exact />
+                    <Route path="/gallery" component={Gallery} />
+                    <Route path="/shop" component={Shop} />
+                    <Route path="/about" component={About} />
+
+                    {/* Using the `render` prop */}
+                    {/* <Route
+                            path="/products/:id"
+                            render={({ match }) => <BlogPost match={match} />}
+                        /> */}
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
