2e3bd2d35faad8a8e6f8bdeb490625c7e888eef2
frozenbanana <henrybergstrom@protonmail.com>
2020-06-29 23:32:58 +0200

    Added scss to override bootstrap

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import { NavigationBar } from "./components/NavigationBar";
 import { About } from "./components/About";
 import { Gallery } from "./components/Gallery";
 import { Shop } from "./components/Shop";
-
-import "./App.css";
+import { Contact } from "./components/Contact";
+import { Container } from "react-bootstrap";
+import "./css/app.css";
 
 // In the code below, BlogPost is used as both a <Route component>
 // and in a <Route render> function. In both cases, it receives a `match`
@@ -16,24 +17,33 @@ import "./App.css";
 //     // ...
 // }
 
+const navLinks = [
+    { name: "Gallery", url: "/gallery" },
+    { name: "Shop", url: "/shop" },
+    { name: "About", url: "/about" },
+    { name: "Contact", url: "/contact" },
+];
 function App() {
     return (
         <Router>
             <div>
-                <NavigationBar title="Maija Papazi" />
-                <Switch>
-                    {/* Using the `component` prop */}
-                    <Route path="/" component={Gallery} exact />
-                    <Route path="/gallery" component={Gallery} />
-                    <Route path="/shop" component={Shop} />
-                    <Route path="/about" component={About} />
+                <NavigationBar title="Maija Papazi" links={navLinks} />
+                <Container>
+                    <Switch>
+                        {/* Using the `component` prop */}
+                        <Route path="/" component={Gallery} exact />
+                        <Route path={navLinks[0].url} component={Gallery} />
+                        <Route path={navLinks[1].url} component={Shop} />
+                        <Route path={navLinks[2].url} component={About} />
+                        <Route path={navLinks[3].url} component={Contact} />
 
-                    {/* Using the `render` prop */}
-                    {/* <Route
+                        {/* Using the `render` prop */}
+                        {/* <Route
                             path="/products/:id"
                             render={({ match }) => <BlogPost match={match} />}
                         /> */}
-                </Switch>
+                    </Switch>
+                </Container>
             </div>
         </Router>
     );
